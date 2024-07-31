7d1751d38b9e2deafd96641effc816f6b8094ac8
ruchi panchal <ruchimpanchal@gmail.com>
2024-07-31 15:31:35 +0530

    hi

---
+++ Controllers/GitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testingCommandGithub.Controllers
+{
+    public class GitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testingCommandGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
